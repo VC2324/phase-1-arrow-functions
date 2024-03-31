0ba147739a1c9fb816d7e47dc4fd3e7ff193c7c3
Victor <victorac2324@gmail.com>
2024-03-30 22:24:28 -0400

    used arrow functions to do arithmatic

---
+++ index.js
@@ -0,0 +1,18 @@
+// const arr=[3,4,9,6,10]
+// function doubleNumbers(arr){
+
+//     const double = arr.map((x)=> x * 2)
+//     return double
+// }
+
+// console.log (doubleNumbers(arr))
+
+const divide = function (){
+    const result = 2000 / 100
+    return result
+}
+ const square = (num1) => num1 * num1;
+ square(3)
+
+ const add = (num1, num2) => num1 + num2;
+ add(9, 9)
